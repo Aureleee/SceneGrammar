48a84775d8fccad71b33a92f2e55257dd3c1e017
Aurèle Habbard <aurelehabbard@gmail.com>
2026-02-08 12:41:32 +0100

    adding save features

--- main.py
+++ main.py
@@ -6,12 +6,18 @@
 #    By: aurele <aurele@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/02/05 21:31:40 by aurele            #+#    #+#              #
-#    Updated: 2026/02/08 11:33:49 by aurele           ###   ########.fr        #
+#    Updated: 2026/02/08 12:39:57 by aurele           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from utils import *
+import importlib
+import utils 
+importlib.reload(utils)
 
-generator = SemanticPromptGenerator()
+
+generator = utils.SemanticPromptGenerator()
 prompt = generator.generate_prompt()
 print(prompt)
+
+with open("saved_prompts.txt", "a") as f:
+    f.write(prompt + "\n")
